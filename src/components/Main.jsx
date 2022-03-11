d49e9dbb84b7a7b26a2ea217540b8b95bd77f57c
Taylor Kirkes <kirkes21@gmail.com>
2022-03-11 10:26:50 -0600

    get posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Main = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        "https://strangers-things.herokuapp.com/api/2202-ftb-et-web-ft/posts"
+      );
+      const data = await response.json();
+      console.log("checking: ", data);
+      setPosts(data.data.posts);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div>
       <h1>Hello from main</h1>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <div>{post.description}</div>
+        </div>
+      ))}
     </div>
   );
 };
